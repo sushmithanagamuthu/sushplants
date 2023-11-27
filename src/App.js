cf82cec6b4681221f89b90ade1e4ae8a5767a21c
sushmithanagamuthu <727722euit197@skcet.ac.in>
2023-11-27 19:13:06 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import SignUp from "./pages/Account/SignUp";
 import Cart from "./pages/Cart/Cart";
 import Contact from "./pages/Contact/Contact";
 import Home from "./pages/Home/Home";
-
+import Journal from "./pages/Journal/Journal";
 import Offer from "./pages/Offer/Offer";
 import Payment from "./pages/payment/Payment";
 import ProductDetails from "./pages/ProductDetails/ProductDetails";
@@ -43,6 +43,7 @@ const router = createBrowserRouter(
         <Route index element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/journal" element={<Journal />}></Route>
 
         {/* ==================== Header Navlink End here ===================== */}
         <Route path="/offer" element={<Offer />}></Route>
